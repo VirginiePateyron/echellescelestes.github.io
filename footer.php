06fe730d43bf91995379b4bc6db3d626568bee43
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-03 19:31:16 +0100

    modification du fichier footer.php avec structure complète ; modification du fichier front-page html vers php pour avoir la version dynamique du site ; ajout de get_header et get_footer dans le fichier front-page pour appeler les fichiers header et footer.php ; modification du fichier header avec les données actualisées du menu de navigation

--- footer.php
+++ footer.php
@@ -1,22 +1,31 @@
-<footer>
-    <div class="container">
-        <article class="footer-content">
-            <section class="footer-content-element">
-                <img src="" alt="">
-                <p></p>
-            </section>
-            <section class="footer-content-element">
-                <img src="" alt="">
-                <p></p>
-            </section>
-            <section class="footer-content-element socialmedia">
-                <img src="" alt="">
-                <img src="" alt="">
-                <img src="" alt="">
-            </section>
-        </article>
-    </div>
-</footer>
+ <!-- verifier comment structurer html du footer -->
+ <footer>
+            <div class="container">
+                <article class="footer-content">
+                    <section class="footer-content-element">
+                        <img src="" alt="">
+                        <p>Installation créée par Léna d’azy
+                            Adresse Bègles
+                            00.00.00.00.00.
+                            Lenadazy.fr
+                        </p>
+                    </section>
+                    <section class="footer-content-element">
+                        <img src="" alt="">
+                        <p>Avec le labo des cultures
+                            Adresse Bègles
+                            00.00.00.00.00.
+                            lelabodescultures.fr
+                        </p>
+                    </section>
+                    <section class="footer-content-element socialmedia">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                        <img src="" alt="">
+                    </section>
+                </article>
+            </div>
+        </footer>
 
 <?php wp_footer(); ?>
 <!-- Vous pouvez ajouter votre script Google Analytics ici -->
--- front-page.php
+++ front-page.php
@@ -1,31 +1,5 @@
+<?php get_header(); ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="background">
-        <div id="home">
-            <header class="main-header container">
-                
-                <h1><a href="#" class="logo">Echelles Célestes</a></h1>
-
-                <nav class="header-nav">
-                    <ul>
-                        <li><a href="#first">Installation</a></li>
-                        <li><a href="#second">Inspirations</a></li>
-                        <li><a href="#third">Portraits</a></li>
-                        <li><a href="#fourth">Jeu</a></li>
-                        <li><a href="#fifth">Ressources</a></li>
-                        <li><a href="#sixth">L'équipe</a></li>
-                        <li><a href="#seven">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
 <!-- Afficher la lune ou plusieurs étoiles, au survol, la lune grossit et au clic, le texte apparaît. Les lunes ou étoiles peuvent être déplacés par l'utilisateur -->
             <img src="" alt="">
         </div>
@@ -730,38 +704,8 @@
             </article>
         </main>
 
-        <!-- verifier comment structurer html du footer -->
-        <footer>
-            <div class="container">
-                <article class="footer-content">
-                    <section class="footer-content-element">
-                        <img src="" alt="">
-                        <p>Installation créée par Léna d’azy
-                            Adresse Bègles
-                            00.00.00.00.00.
-                            Lenadazy.fr
-                        </p>
-                    </section>
-                    <section class="footer-content-element">
-                        <img src="" alt="">
-                        <p>Avec le labo des cultures
-                            Adresse Bègles
-                            00.00.00.00.00.
-                            lelabodescultures.fr
-                        </p>
-                    </section>
-                    <section class="footer-content-element socialmedia">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                    </section>
-                </article>
-            </div>
-        </footer>
-
-
     </div>
-</body>
-</html>
+
+<?php get_footer(); ?>
 
 
