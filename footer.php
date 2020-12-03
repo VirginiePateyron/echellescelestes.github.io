fd984ceb92f1ff02ebfc06f6b4fe5b88dd23e5ff
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-03 18:22:33 +0100

    initialisation html du fichier footer avant intégration wp

--- footer.php
+++ footer.php
@@ -1,3 +1,23 @@
+<footer>
+    <div class="container">
+        <article class="footer-content">
+            <section class="footer-content-element">
+                <img src="" alt="">
+                <p></p>
+            </section>
+            <section class="footer-content-element">
+                <img src="" alt="">
+                <p></p>
+            </section>
+            <section class="footer-content-element socialmedia">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </section>
+        </article>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 <!-- Vous pouvez ajouter votre script Google Analytics ici -->
 </body>
