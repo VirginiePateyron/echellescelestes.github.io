9ea148c1b16ef5c8bc0e47ea33f0036456d234bc
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-03 17:47:30 +0100

    envoi du fichier front-page en cours de la one page html en cours

--- front-page.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-
-
-
-
-<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -11,23 +11,25 @@
     
     <?php wp_body_open(); ?>
 
-    <div id="home">
-        <header class="main-header container">
-            
-            <h1><a href="#" class="logo">Echelles Célestes</a></h1>
+    <div class="background">
+        <div id="home">
+            <header class="main-header container">
+                
+                <h1><a href="#" class="logo">Echelles Célestes</a></h1>
 
-            <nav class="header-nav">
-                <ul>
-                    <li><a href="first">Installation</a></li>
-                    <li><a href="second">Inspirations</a></li>
-                    <li><a href="third">Portraits</a></li>
-                    <li><a href="fourth">Jeu</a></li>
-                    <li><a href="fifth">Ressources</a></li>
-                    <li><a href="sixth">L'équipe</a></li>
-                    <li><a href="seventh">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
-    </div>
+                <nav class="header-nav">
+                    <ul>
+                        <li><a href="first">Installation</a></li>
+                        <li><a href="second">Inspirations</a></li>
+                        <li><a href="third">Portraits</a></li>
+                        <li><a href="fourth">Jeu</a></li>
+                        <li><a href="fifth">Ressources</a></li>
+                        <li><a href="sixth">L'équipe</a></li>
+                        <li><a href="seventh">Contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    
 
     
