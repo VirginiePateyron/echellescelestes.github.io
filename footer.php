d7ea1aad90f68a81727de64169a1d066e9c4ae88
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-04 16:36:01 +0100

    ajout des repertoires images au dossier ; modification du css et de la structure html avec l'extension du container dans le header et la sortie de la div comprenant le background du site du footer

--- footer.php
+++ footer.php
@@ -1,31 +1,32 @@
  <!-- verifier comment structurer html du footer -->
- <footer>
-            <div class="container">
-                <article class="footer-content">
-                    <section class="footer-content-element">
-                        <img src="" alt="">
-                        <p>Installation créée par Léna d’azy
-                            Adresse Bègles
-                            00.00.00.00.00.
-                            Lenadazy.fr
-                        </p>
-                    </section>
-                    <section class="footer-content-element">
-                        <img src="" alt="">
-                        <p>Avec le labo des cultures
-                            Adresse Bègles
-                            00.00.00.00.00.
-                            lelabodescultures.fr
-                        </p>
-                    </section>
-                    <section class="footer-content-element socialmedia">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                        <img src="" alt="">
-                    </section>
-                </article>
-            </div>
-        </footer>
+    <footer>
+        <div class="container">
+            <article class="footer-content">
+                <section class="footer-content-element">
+                    <img src="" alt="">
+                    <p>Installation créée par Léna d’azy
+                        Adresse Bègles
+                        00.00.00.00.00.
+                        Lenadazy.fr
+                    </p>
+                </section>
+                <section class="footer-content-element">
+                    <img src="" alt="">
+                    <p>Avec le labo des cultures
+                        Adresse Bègles
+                        00.00.00.00.00.
+                        lelabodescultures.fr
+                    </p>
+                </section>
+                <section class="footer-content-element socialmedia">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </section>
+            </article>
+        </div>
+    </footer>
+</div>
 
 <?php wp_footer(); ?>
 <!-- Vous pouvez ajouter votre script Google Analytics ici -->
--- header.php
+++ header.php
@@ -10,30 +10,38 @@
 
 <body <?php body_class(); ?>>
     
-    <?php wp_body_open(); ?>
+<?php wp_body_open(); ?>
 
-    <div class="background">
-        <div id="home">
-            <header class="main-header container">
-                
-                <h1><a href="#" class="logo">Echelles Célestes</a></h1>
-
-                <nav class="header-nav">
-                    <ul>
-                        <li><a href="#first">Installation</a></li>
-                        <li><a href="#second">Inspirations</a></li>
-                        <li><a href="#third">Portraits</a></li>
-                        <li><a href="#fourth">Jeu</a></li>
-                        <li><a href="#fifth">Ressources</a></li>
-                        <li><a href="#sixth">L'équipe</a></li>
-                        <li><a href="#seventh">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
+<div class="background">
+    <div id="home" class="container">
+        <header class="main-header">
             
-             <!-- Afficher la lune ou plusieurs étoiles, au survol, la lune grossit et au clic, le texte apparaît. Les lunes ou étoiles peuvent être déplacés par l'utilisateur -->
-            <img src="" alt="">
-        </div>
+            <h1><a href="#" class="logo">Echelles Célestes</a></h1>
+
+            <nav class="header-nav">
+                <ul>
+                    <li><a href="#first">Installation</a></li>
+                    <li><a href="#second">Inspirations</a></li>
+                    <li><a href="#third">Portraits</a></li>
+                    <li><a href="#fourth">Jeu</a></li>
+                    <li><a href="#fifth">Ressources</a></li>
+                    <li><a href="#sixth">L'équipe</a></li>
+                    <li><a href="#seventh">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+            <!-- Afficher la lune ou plusieurs étoiles, au survol, la lune grossit et au clic, le texte apparaît. Les lunes ou étoiles peuvent être déplacés par l'utilisateur -->
+            <?php 
+            $image = get_field('hero_img');
+            $size = 'full'; // (thumbnail, medium, large, full or custom size)
+            if( $image ) {
+                echo wp_get_attachment_image( $image, $size ); 
+            } else { ?>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/noimg.png" alt="Image par défault">
+            <?php }; ?>
+        
+    </div>
        
     
 
