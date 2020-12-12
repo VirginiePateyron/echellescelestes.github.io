2b4da63b10cc8af7717573de80830c459fcec1f7
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-12 20:32:34 +0100

    ajout d'une fonction pour afficher la fenetre modale de la section des portraits. Réussi avec vanilla JS. Modification du dom avec ajout de classe pour activer le script

--- echellescelestes-web/js/script.js
+++ echellescelestes-web/js/script.js
@@ -1,3 +1,5 @@
+/* création d'une fonction qui déplace le menu de navigation à droite au scroll une fois passé le héro */
+
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -7,14 +9,20 @@ var menuItem = document.getElementsByClassName("menu-item");
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
+    navbar.classList.add("sticky"); // ajouter une classe à l'élément id = navbar
+    document.getElementById("header").style.fontSize = "6rem";  // redimensionner le titre h1 au scroll
+    document.getElementById("header").style.fontWeight = "lighter"; // changer la graisse de la police au scroll
     for(let i = 0; i < menuItem.length; i++){
-      menuItem[i].classList.add("positionOff")
+      menuItem[i].classList.add("positionOff") //ajouter une classe à l'élement avec la classe menu-item qui est une liste donc appeler un tableau pour prendre en compte tous les éléments de liste avec cette classe
     }
   } else {
     navbar.classList.remove("sticky")
+    document.getElementById("header").style.fontSize = "12rem"; // revenir aux propriétés définies sur le titre h1 dans le CSS en revenant en haut du site
+    document.getElementById("header").style.fontWeight = "initial"; // revenir aux propriétés définies sur le titre h1 dans le CSS en revenant en haut du site
     for(let i = 0; i < menuItem.length; i++){
       menuItem[i].classList.remove("positionOff")
     }
   }
 }
+
+
