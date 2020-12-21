3ab2890863d98d8563d67f5498a6484502bef2a4
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-21 16:02:58 +0100

    modification css des sections : nav constellation avec amélioration des marges; réduction des marges;

--- echellescelestes-web/js/script.js
+++ echellescelestes-web/js/script.js
@@ -10,14 +10,14 @@ const menuItem = document.getElementsByClassName("menu-item");
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky"); // ajouter une classe à l'élément id = navbar
-    document.getElementById("header").style.fontSize = "6rem";  // redimensionner le titre h1 au scroll
+    document.getElementById("header").style.fontSize = "4rem";  // redimensionner le titre h1 au scroll
     document.getElementById("header").style.fontWeight = "lighter"; // changer la graisse de la police au scroll
     for(let i = 0; i < menuItem.length; i++){
       menuItem[i].classList.add("positionOff") //ajouter une classe à l'élement avec la classe menu-item qui est une liste donc appeler un tableau pour prendre en compte tous les éléments de liste avec cette classe
     }
   } else {
     navbar.classList.remove("sticky")
-    document.getElementById("header").style.fontSize = "12rem"; // revenir aux propriétés définies sur le titre h1 dans le CSS en revenant en haut du site
+    document.getElementById("header").style.fontSize = "10rem"; // revenir aux propriétés définies sur le titre h1 dans le CSS en revenant en haut du site
     document.getElementById("header").style.fontWeight = "initial"; // revenir aux propriétés définies sur le titre h1 dans le CSS en revenant en haut du site
     for(let i = 0; i < menuItem.length; i++){
       menuItem[i].classList.remove("positionOff")
