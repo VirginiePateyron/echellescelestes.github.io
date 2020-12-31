82bbec5ae07b35bc9ffebad7c41eaf8f53c01226
VirginiePateyron <virginie.pateyron@gmail.com>
2020-12-31 15:35:17 +0100

    init theme wordpress

---
+++ footer.php
@@ -0,0 +1,4 @@
+<?php wp_footer(); ?>
+<!-- Vous pouvez ajouter votre script Google Analytics ici -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    
+    <?php wp_body_open(); ?>
