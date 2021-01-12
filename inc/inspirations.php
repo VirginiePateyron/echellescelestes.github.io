7f1cac6b45a2c960cb235b655a17718f311e08f4
VirginiePateyron <virginie.pateyron@gmail.com>
2021-01-12 22:49:32 +0100

    modif du template inspirations avec champs dynamiques ACF

--- inc/inspirations.php
+++ inc/inspirations.php
@@ -4,19 +4,22 @@
      <img class="img-deco-bg" src="<?php echo THEME_DIR_IMG; ?>/astro/lowell_obs.jpg" alt="l'astronome Lowell observe l'espace avec une lunette astronomique" />
     <!-- <h2 class="gradient">inspirations</h2> -->
     <h2 class="buttons gradient">
-        <button class="draw meet">inspirations</button>
+        <button class="draw meet"><?php the_field('second_section_title'); ?></button>
     </h2>
 
-    <p class="h2-subtitle"> D'après un livre et un documentaire</p>
+    <p class="h2-subtitle"> <?php the_field('second_section_text'); ?></p>
 
     <div class="container">
 
         <div class="second-section-content">
             <section class="second-section-content-element">
                 <div class="second-section-content-element-col">
+                <?php if(have_rows('second_section_first_area')):
+                while(have_rows('second_section_first_area')): 
+                    the_row();?>
                     <div class="second-section-content-element-col-child second-section-content-element-col-child-text">
-                        <h3>l’extension narrative d’un ouvrage</h3>
-                        <p>Le livre « Ciel ! Mon étoile » de Cécile Léna (illustratrice), Pierre Léna (astrophysicien) et Michel Serres (philosophe et historien des sciences), nous offre un voyage dans les observatoires. <br><br>Dans ce carnet de voyage pas comme les autres, ils reviennent sur la naissance des observatoires astronomiques sur les cinq continents et sur le rapport des hommes à l’univers qui les entoure. Le lecteur y découvre des instruments fascinants qui nous permettent de comprendre l’univers et en révèlent toute sa beauté. <br><br>Aujourd’hui, ils nous font voir le passé et, un jour, nous permettront peut-être de remonter le temps pour percer le mystère de nos origines.</p>
+                        <h3><?php the_sub_field('second_section_first_area_title'); ?></h3>
+                        <p><?php the_sub_field('second_section_first_area_text'); ?></p>
                     </div>
                     <div class="second-section-content-element-col-child second-section-content-element-col-child-img" data-aos="fade-left" data-aos-offset="300"
                     data-aos-easing="ease-in-sine" data-aos-duration="2000">
@@ -27,16 +30,18 @@
                         </div>
                         <img src="" alt="">
                     </div>
+                    <?php endwhile; 
+                endif; ?>
                 </div>
             </section>
             <section class="second-section-content-element">
                 <div class="second-section-content-element-col second-section-content-element-col-reverse">
+                <?php if(have_rows('second_section_second_area')):
+                while(have_rows('second_section_second_area')): 
+                    the_row();?>
                     <div class="second-section-content-element-col-child second-section-content-element-col-child-text">
-                        <h3>la narration transmédia d’un documentaire</h3>
-                        <p>Quelle est notre place dans l’univers &nbsp;? Y sommes-nous seuls &nbsp;? Comment se forment les planètes, les étoiles, les galaxies &nbsp;? Qu’est-ce qui les tient ensemble &nbsp;? C’est quoi, une naine blanche, une géante rouge, un trou noir &nbsp;? <br><br>
-                            Une série en dix épisodes dans laquelle Robert Pansard-Besson conte l’histoire des observatoires astronomiques et de leurs découvertes et nous invite à parcourir le ciel et la terre.<br><br>
-                            Réalisateur : Robert Pansard-Besson
-                            </p>
+                        <h3><?php the_sub_field('second_section_second_area_title'); ?></h3>
+                        <p><?php the_sub_field('second_section_second_area_text'); ?></p>
                     </div>
                     <div class="second-section-content-element-col-child second-section-content-element-col-child-img" data-aos="fade-right" data-aos-offset="300"
                     data-aos-easing="ease-in-sine" data-aos-duration="2000">
@@ -46,6 +51,8 @@
                             <a href="https://www.lalibrairie.com/livres/tours-du-monde-tours-du-ciel-2_0-576712_9782759804979.html?ctx=8dede6a1667b1196b8d08bbbc36680c1" target="_blank">Acquérir le documentaire</a>
                         </div>
                     </div>
+                    <?php endwhile; 
+                endif; ?>
                 </div>
             </section>
         </div>
